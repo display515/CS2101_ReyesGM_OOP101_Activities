d07d0ad5a90e2fe74ddcde6886d1557ad5aa1b12
Display <martinireyes15@gmail.com>
2026-09-09 22:03:03 +0800

    Setup

---
+++ Canteen.java
@@ -0,0 +1,9 @@
+public class Canteen {
+    public static void main(String[] args) {
+        printMenu();
+    }
+
+    public static void printMenu() {
+        System.out.println("=====\tMenu\t=====");
+    }
+}
