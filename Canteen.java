010c6cbd88fe5a211a5979ed77a06fb3d035a76e
Display <martinireyes15@gmail.com>
2026-09-10 09:16:12 +0800

    Added printMenu

--- Canteen.java
+++ Canteen.java
@@ -2,6 +2,11 @@
 
 public class Canteen{
     public static void main(String[] args){
+        printMenu();
 
+        }
+    
+    public static void printMenu(){
+        System.out.println("====\tMenu\t====");
     }
 }
